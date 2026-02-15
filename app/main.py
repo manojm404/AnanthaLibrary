aa9f2a5f7748fd16eea7d807306f45ae1966f95f
Iamanojsharma <manojsharmayandapally@MacBookPro.lan>
2026-02-15 17:54:51 -0600

    Initial commit - AnanthaLibrary base setup

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <html>
+        <head>
+            <title>AnanthaLibrary</title>
+        </head>
+        <body>
+            <h1>Welcome to AnanthaLibrary 🚀</h1>
+        </body>
+    </html>
+    """
